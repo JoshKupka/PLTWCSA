17e91da74d94bec446976b4d4947731008294a0c
JoshKupka <josh.kupka@gmail.com>
2018-03-28 07:59:28 -0500

    Create Fish.java

---
+++ 224/Animal App/Fish.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Fish here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Fish extends Animal
+{
+    public Fish()
+    {
+        
+    }
+    
+    public String say()
+    {
+        return "Blub";
+    }
+    
+    public String toString(){
+        return "Fish";
+    }
+}
