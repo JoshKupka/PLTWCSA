ae19f1e0d6a57d8d3ce9ffe07db58d64a4d07b23
JoshKupka <josh.kupka@gmail.com>
2018-03-28 07:57:35 -0500

    Create Seal.java

---
+++ 224/Animal App/Seal.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Seal here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Seal extends Animal implements Fun
+{
+    public Seal(){
+    }
+
+    public String say(){
+        return " Baby, I compare you to a kiss from a rose on the grey Ooh, the more I get of you the stranger it feels, yeah Now that your rose is in bloom A light hits the gloom on the grey ";
+    }
+
+    public String toString() {
+        return "Seal";
+    }
+
+    public String play(){
+        return "The seal bounces a ball off its nose.";
+    }
+}
