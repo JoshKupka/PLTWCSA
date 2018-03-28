f163d950525a9ffb608fa2549d968938f5021547
JoshKupka <josh.kupka@gmail.com>
2018-03-28 08:00:23 -0500

    Create Duck.java

---
+++ 224/Animal App/Duck.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Duck here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Duck extends Animal
+{
+    // instance variables - replace the example below with your own
+    public Duck()
+    {
+
+    }
+    
+    public String say()
+    {
+        return "Quack";
+    }
+    
+    public String toString(){
+        return "Duck";
+    }
+}
