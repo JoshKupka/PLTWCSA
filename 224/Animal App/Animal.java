6831a2e179b3c738ee9156cb8597d4d2da1a91bc
JoshKupka <josh.kupka@gmail.com>
2018-03-28 07:56:57 -0500

    Create Animal.java

---
+++ 224/Animal App/Animal.java
@@ -0,0 +1,13 @@
+
+/**
+ * Abstract class Animal - write a description of the class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public abstract class Animal
+{
+    public abstract String say();
+    
+    
+}
